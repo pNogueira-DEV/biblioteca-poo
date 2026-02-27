3232a6871d5d24fae655e5724c888a0cf533a907
Paulo <pauloricardo.st1812@hotmail.com>
2026-02-27 16:16:37 -0300

    classe/ funcoes

--- item_biblioteca.py
+++ item_biblioteca.py
@@ -4,4 +4,55 @@ class ItemBiblioteca:
         self.set_titulo(titulo)
         self.set_ano(ano)
         self.__disponivel = True
+       # Getters
+    def get_codigo(self):
+        return self.__codigo
+
+    def get_titulo(self):
+        return self.__titulo
+
+    def get_ano(self):
+        return self.__ano
+
+    def get_disponivel(self):
+        return self.__disponivel
+
+    # Setters com validação
+    def set_codigo(self, codigo):
+        if codigo:
+            self.__codigo = codigo
+        else:
+            raise ValueError("Código inválido.")
+
+    def set_titulo(self, titulo):
+        if titulo.strip() != "":
+            self.__titulo = titulo
+        else:
+            raise ValueError("Título não pode ser vazio.")
+
+    def set_ano(self, ano):
+        if ano > 0:
+            self.__ano = ano
+        else:
+            raise ValueError("Ano inválido.")
+
+    # Métodos principais
+    def emprestar(self):
+        if self.__disponivel:
+            self.__disponivel = False
+            print(f"Item '{self.__titulo}' emprestado com sucesso!")
+        else:
+            print(f"Item '{self.__titulo}' já está emprestado.")
+
+    def devolver(self):
+        if not self.__disponivel:
+            self.__disponivel = True
+            print(f"Item '{self.__titulo}' devolvido com sucesso!")
+        else:
+            print(f"Item '{self.__titulo}' já está disponível.")
+
+    # Método polimórfico (será sobrescrito)
+    def exibir_detalhes(self):
+        print(f"Código: {self.__codigo} | Título: {self.__titulo} | Ano: {self.__ano}")
+
 
